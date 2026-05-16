8b12acd8eafda65a2b76078a32173e081351f291
ngbal <ngbal@example.com>
2026-05-17 04:08:03 +0700

    feat: remove redundant Preset Name input in Pin Presets grid

--- src/ui.rs
+++ src/ui.rs
@@ -11595,18 +11595,6 @@ impl CrosshairApp {
                     .num_columns(2)
                     .spacing([14.0, 8.0])
                     .show(ui, |ui| {
-                        ui.label(Self::tr_lang(language, "Preset Name", "Preset Name"));
-                        let response =
-                            ui.add_sized([260.0, 24.0], TextEdit::singleline(&mut preset.name));
-                        Self::apply_vietnamese_input_if_changed(
-                            &response,
-                            self.state.vietnamese_input_enabled,
-                            self.state.vietnamese_input_mode,
-                            &mut preset.name,
-                        );
-                        live_sync |= response.changed();
-                        ui.end_row();
-
                         ui.label(Self::tr_lang(language, "Hotkey", "Hotkey"));
                         ui.horizontal_wrapped(|ui| {
                             let capture_target = CaptureRequest::PinPresetHotkey(preset.id);
