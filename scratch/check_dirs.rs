ae9c362c89d5ba49fbf0b9af7cb8f744349f0819
ngbal <ngbal@example.com>
2026-05-22 23:42:49 +0700

    clean: remove obsolete scratch helper files from git

--- scratch/check_dirs.rs
+++
@@ -1,9 +0,0 @@
-use directories::ProjectDirs;
-
-fn main() {
-    if let Some(dirs) = ProjectDirs::from("com", "", "MacroNest") {
-        println!("Data Local Dir: {:?}", dirs.data_local_dir());
-    } else {
-        println!("Could not find project dirs");
-    }
-}
